29bfb49eb68b37329f4169f6922d1f35898dadef
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-12-15 13:16:04 -0700

    Added api endpoint to environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.tsx"),
@@ -40,6 +41,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
+    }),
+    new Dotenv({
+      path: './.env.development',
     }),
   ],
 };
