55b943f737916ebea1d29c0d91194a2d39af3297
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-03-09 14:17:21 -0700

    Minor update

--- src/pages/Crypto/index.tsx
+++ src/pages/Crypto/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'chartjs-adapter-moment';
 // Components
+import { NavBar } from '../../components/NavBar';
 import { Line } from 'react-chartjs-2';
 import {
   Box,
@@ -13,7 +14,6 @@ import {
 import { CryptoData, CryptoPrices } from './Container';
 // Styles
 import './index.css';
-import { NavBar } from '../../components/NavBar';
 
 interface CryptoProps {
   cryptoData: CryptoData | null;
