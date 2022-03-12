d309a21c645c1ead0dc7dc9e4fd95e3fe7e042bf
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-03-11 21:47:59 -0700

    Setup 404 Response

---
+++ src/pages/PageNotFound/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+export const PageNotFound: React.FC = () => {
+  return (
+    <div className="page-not-found-container">
+      <h1>404</h1>
+      <h2>Sorry...</h2>
+      <p>page not found</p>
+    </div>
+  );
+};
