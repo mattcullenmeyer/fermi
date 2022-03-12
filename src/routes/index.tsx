9fde2ab5e209ba782c295dc0fb9338b362fd4762
Matt Cullen-Meyer <mattcullenmeyer@gmail.com>
2022-03-11 21:50:01 -0700

    [TINY-29] Setup 404 Response (#24)
    * Setup 404 Response
    
    * Updated template

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,27 @@
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { Account } from '../pages/Account';
 import { Home } from '../pages/Home';
 import { LoginContainer as Login } from '../pages/Login/Container';
 import { Logout } from '../pages/Logout';
 import { Signup } from '../pages/Signup';
 import { CryptoContainer as Crypto } from '../pages/Crypto/Container';
+import { PageNotFound } from '../pages/PageNotFound';
 
 export const Routes = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/logout" exact component={Logout} />
-      <Route path="/signup" exact component={Signup} />
-      <Route path="/account" exact component={Account} />
-      <Route path="/crypto" exact>
-        <Redirect to="/crypto/bitcoin" />
-      </Route>
-      <Route path="/crypto/:slug" exact component={Crypto} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/logout" exact component={Logout} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/account" exact component={Account} />
+        <Route path="/crypto" exact>
+          <Redirect to="/crypto/bitcoin" />
+        </Route>
+        <Route path="/crypto/:slug" exact component={Crypto} />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
     </BrowserRouter>
   );
 };
