a13bf6efaf8e99986729e5c2e91ebc784231e5ab
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-04-08 09:33:24 -0600

    Refactoring

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useAppSelector } from '../../state/store';
 
-export const Account: React.FC = () => {
+export const Profile: React.FC = () => {
   const { user } = useAppSelector((state) => state);
   console.log(user.data);
-  return <h1>Account</h1>;
+  return <h1>Profile</h1>;
 };
