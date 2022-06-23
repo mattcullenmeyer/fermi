abc149d2a9fec0b991945c287950dfe99e872ae6
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-06-22 22:05:18 -0600

    [TINY-66] Cypress Testing GitHub Action

---
+++ webpack.staging.js
@@ -0,0 +1,37 @@
+const {
+  entry,
+  output,
+  resolve,
+  moduleRules,
+  devServer,
+  plugins,
+} = require("./webpack.config.js");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry,
+  output,
+  mode: "development",
+  resolve,
+  module: {
+    rules: [
+      ...moduleRules,
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+  devServer: {
+    ...devServer,
+    host: "127.0.0.1",
+    port: 8080,
+  },
+  plugins: [
+    ...plugins,
+    new Dotenv({
+      path: "./environment/.env.staging",
+    }),
+  ],
+};
